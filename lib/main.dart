7e45e5b32c08d976b8f93c5283d7f17ca1e69208
Ethan <emanschneider@gmail.com>
2019-12-03 13:30:48 -0500

    improved the class file and added some minor changes to the flutter app over break. changes made and not committed over break beacsuse i forgot

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      theme: ThemeData (
+        primarySwatch: Colors.lime,
+
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -67,28 +59,10 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+        child: Column (
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -101,11 +75,24 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        child: Container(
+          height: 50.0,
+          alignment: Alignment.centerLeft,
+          padding: EdgeInsets.all(5.0),
+          child: Text(
+            'You have pushed the button $_counter times.',
+            textAlign: TextAlign.left,
+          ),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
     );
   }
 }
--- lib/statClasses.dart
+++ lib/statClasses.dart
@@ -4,8 +4,14 @@ class Stat {
   set name (var potentName) {
     name = potentName.toString();
   }
-  Stat (String name) {
+  Stat(String name) {
     this._name = name;
+  }
+  String toJson() {
+    return '''{
+  "name" : "$_name",
+  "type" : "Stat"
+}''';
   }
 }
 
@@ -19,43 +25,129 @@ class Attribute extends Stat {
       return base;
     }
   }
-  Attribute (String name, int base, bool halfRounded) : super(name) {
+  Attribute(String name, int base, bool halfRounded) : super(name) {
     this.base = base;
     this.halfRounded = halfRounded;
+  }
+  Attribute.fromAttr(Attribute attribute) : super(attribute.name) {
+    this.base = attribute.base;
+    this.halfRounded = attribute.halfRounded;
+  }
+  String toJson() {
+    return '''{
+  "name" : "$_name",
+  "type" : "Attribute",
+  "base" : $base,
+  "halfRounded" : $halfRounded
+}''';
   }
 }
 
 class Skill extends Stat {
-  Attribute attribute;
-  int _mod;
-  set mod (int newMod) {
-    _mod = newMod;
+  String relatedAttribute;
+  int mod;
+  Skill(String name, String attributeName, int mod) : super (name) {
+    this.relatedAttribute = attributeName;
+    this.mod = mod;
   }
-  get mod => _mod + attribute.mod;
-  Skill (String name, Attribute attribute, int mod) : super (name) {
-    this.attribute = attribute;
-    this._mod = mod;
+  Skill.fromSkill(Skill copy) : super (copy.name) {
+    this.relatedAttribute = copy.relatedAttribute;
+    this.mod = mod;
+  }
+  String toJson() {
+    return '''{
+  "name" : "$_name",
+  "type" : "Skill",
+  "relatedAttribute" : $relatedAttribute,
+  "mod" : $mod
+}''';
   }
 }
 class Pool extends Stat {
   int max;
   int _pointer;
   get pointer => _pointer;
-  Pool (String name, int max) : super(name) {
+  Pool(String name, int max) : super(name) {
     this.max = max;
     _pointer = max;
   }
-  decreasePointer() {
-    if (_pointer > 0){
-      _pointer--;
+  Pool.fromPool(Pool copy) : super(copy.name){
+    this.max = copy.max;
+    _pointer = copy.pointer;
+  }
+  decreasePointer([int amount]) {
+    for (amount ??= 1; amount>0; amount--) {
+      if (_pointer > 0) {
+        _pointer--;
+      }
     }
   }
-  increasePointer() {
-    if (_pointer < max) {
-      _pointer++;
+  increasePointer([int amount]) {
+    for (amount ??=1; amount > 0; amount--) {
+      if (_pointer < max) {
+        _pointer++;
+      }
     }
   }
   resetPointer() {
     _pointer = max;
   }
+  String toJson() {
+    return '''{
+  "name" : "$_name",
+  "type" : "Pool",
+  "max" : $max,
+  "pointer" : $pointer
+}''';
+  }
+}
+
+class Character {
+  String name;
+  Map<String, Attribute> attrs;
+  Map<String, Skill> skills;
+  Map<String, Pool> pools;
+  
+  Character (String name, Map<String, Stat> importStats) {
+    this.name = name;
+    importStats.forEach((key, value) {
+      if (value is Attribute) {
+        attrs[key] = value;
+      }
+      else if (value is Skill) {
+        skills[key] = value;
+      }
+      else if (value is Pool) {
+        pools[key] = value;
+      }
+    });
+    if (!pools.containsKey("HP")){
+      pools["HP"] = new Pool("HP", 10);
+    }
+  }
+
+  String toJson() {
+    var toReturn = ''''{
+  "name" : "$name",
+  "attributes" : {
+    ''';
+    attrs.forEach((key, value) {
+      toReturn += '"' + key + '" : ' + value.toJson() + "," ;
+    });
+    toReturn += '''}, 
+  "skills" : {
+    ''';
+    skills.forEach((key, value) {
+      toReturn += '"' + key + '" : ' + value.toJson() + "," ;
+    });
+    toReturn += '''}, 
+  "pools" : {
+    ''';
+    pools.forEach((key, value) {
+      toReturn += '"' + key + '" : ' + value.toJson() + "," ;
+    });
+    toReturn += '''}''';
+    return toReturn;
+  }
+
 }
